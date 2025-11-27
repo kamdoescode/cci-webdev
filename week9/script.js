3edda03638fb19171152b855e2f3e48e33f0a35f
kamdoescode <kamryn.hanley@gmail.com>
2025-11-27 15:55:51 +0000

    trying out animations with GSAP. using timelines to control animations.

--- week9/script.js
+++ week9/script.js
@@ -1,11 +1,20 @@
 // create a variable for the div
-const box = document.querySelector(".myDiv")
+// const box = document.querySelector(".myDiv")
 // create variable for the button
-const btn = document.querySelector("#btn");
+// const btn = document.querySelector("#btn");
 
 //using the activate button to move the box 50px to the right
-btn.addEventListener("click", function() {
-  const currentLeft = parseInt (getComputedStyle(box).left);
-  box.style.left = currentLeft + 50 + "px";
-});
+// btn.addEventListener("click", function() {
+//   const currentLeft = parseInt (getComputedStyle(box).left);
+//   box.style.left = currentLeft + 50 + "px";
+// });
 
+let tl = gsap.timeline();
+
+tl.to(".myBox", {x: 200, duration: 1 })
+.to(".myBox", {rotation: 360, duration: 2})
+
+// gsap.to(".myBox", {
+//     x: 200,
+//     duration: 1,
+// })
